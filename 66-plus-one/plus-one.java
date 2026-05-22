d22366202a99e8233dc174a58f2140090cc3afb3
Kaveen Wickramasinghe <117672888+KaveenW@users.noreply.github.com>
2026-05-23 00:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (77.20%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,23 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
-
-    // Start from the end of the array
-    for (int i = n - 1; i >= 0; i--) {
-        // If the current digit is less than 9, just increment and return
-        if (digits[i] < 9) {
-            digits[i]++;
-            return digits;
-        }
         
-        // If it's 9, it becomes 0 and the loop continues to the next digit
-        digits[i] = 0;
-    }
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
 
-    // If we get here, it means all digits were 9 (e.g., 999 -> 1000)
-    int[] newNumber = new int[n + 1];
-    newNumber[0] = 1; 
-    // The rest of the array is already 0 by default in Java
-    return newNumber;
+            digits[i] = 0;
+        }
+
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+
+        return newDigits;
     }
 }
